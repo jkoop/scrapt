e2fa81f8eaac8b42127bf4baface112661138d18
Joe Koop <joe@joekoop.com>
2022-08-11 14:35:38 -0500

    add signing

--- scrapt.sh
+++ scrapt.sh
@@ -19,4 +19,6 @@ apt-ftparchive packages . > Packages
 echo "Created Packages file"
 
 apt-ftparchive release . > Release
+gpg --armor --detach-sign Release && mv Release.asc Release.gpg
+gpg --clear-sign Release && mv Release.asc InRelease
 echo "Created Release file"
